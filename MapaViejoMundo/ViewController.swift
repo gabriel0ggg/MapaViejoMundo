6a05d82a03a866538f0486c941342c035137bceb
Gabriel Guevara Gutierrez <gabriel0ggg@gmail.com>
2020-03-12 13:43:55 -0600

    Version1

--- MapaViejoMundo/ViewController.swift
+++ MapaViejoMundo/ViewController.swift
@@ -7,14 +7,56 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
+
+    let locationManager = CLLocationManager()
+    @IBOutlet weak var mapview: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.locationManager.requestAlwaysAuthorization()
+        // For use in foreground
+        self.locationManager.requestWhenInUseAuthorization()
+
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.startUpdatingLocation()
+        }
+
+        mapview.delegate = self
+        mapview.mapType = .satellite
+        mapview.isZoomEnabled = true
+        mapview.isScrollEnabled = true
+
+        if let coor = mapview.userLocation.location?.coordinate{
+            mapview.setCenter(coor, animated: true)
+        }
         // Do any additional setup after loading the view.
     }
 
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let locValue:CLLocationCoordinate2D = manager.location!.coordinate
+
+        mapview.mapType = MKMapType.satellite
+
+        let span = MKCoordinateSpan(latitudeDelta: 0.005, longitudeDelta: 0.005)
+        let region = MKCoordinateRegion(center: locValue, span: span)
+        mapview.setRegion(region, animated: true)
+
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = locValue
+        annotation.title = "Titulo"
+        annotation.subtitle = "Ubicacion Actual"
+        mapview.addAnnotation(annotation)
+
+        //centerMap(locValue)
+    }
 
 }
 
